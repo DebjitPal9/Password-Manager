d5aeb1908b4e362b655523311e12535512bc3db8
Debjit Pal <63508896+DebjitPal9@users.noreply.github.com>
2024-06-20 14:45:22 +0530

    Changed Copyright

--- PasswordManager/src/components/Footer.jsx
+++ PasswordManager/src/components/Footer.jsx
@@ -6,7 +6,7 @@ const Footer = () => {
 
 <footer className="rounded-lg shadow m-4 dark:bg-gray-800 max-w-[95.5vw]">
     <div className="h-100 mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
-      <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2024 <a href="https://github.com/DebjitPal9" className="hover:underline">PassMan™</a>. No Rights Reserved.
+      <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2024 <a href="https://github.com/DebjitPal9" className="hover:underline">PassMan™</a>. All Rights Reserved.
     </span>
     <ul className="flex flex-wrap items-center mt-3 text-sm font-medium text-gray-500 dark:text-gray-400 sm:mt-0">
         <li>
